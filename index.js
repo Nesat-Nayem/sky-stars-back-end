b4df272d6c71592ee90478b86776289d06ebdb87
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2022-01-02 17:51:06 +0600

    added service add delete add order add delete manage api

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 const cors = require('cors');
 
-
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -22,20 +21,102 @@ async function run() {
 
         // database and collection
         const database = client.db("skyDB");
-        const servicesCollection = database.collection('services');
+        const servicesCollection = database.collection("services");
+        const reviewsCollection = database.collection("reviews");
+        const ordersCollection = database.collection("orders");
+        const usersCollections = database.collection("users");
 
-        // insert Services
+        // insert Service
         app.post('/addService', async (req, res) => {
             const service = req.body;
             const result = await servicesCollection.insertOne(service);
             res.send(result);
         })
 
-        // read read service
+        // read Service
         app.get('/service', async (req, res) => {
             const result = await servicesCollection.find({}).toArray();
             res.send(result)
         })
+
+        // add Review
+        app.post('/addReview', async (req, res) => {
+            const review = req.body;
+            const result = await reviewsCollection.insertOne(review);
+            res.send(result);
+        })
+
+        // read review
+        app.get('/review', async (req, res) => {
+            const review = await reviewsCollection.find({}).toArray();
+            res.send(review)
+        })
+
+        // add order
+        app.post('/addOrder', async (req, res) => {
+            const order = req.body;
+            const result = await ordersCollection.insertOne(order);
+            res.json(result);
+        })
+
+        // read myOrders
+        app.get('/MyOrders/:email', async (req, res) => {
+            const email = req.params.email;
+            const result = await ordersCollection.find({ CustomerEmail: email }).toArray();
+            res.send(result)
+        })
+
+        //order delete from  myOrders
+        app.delete('/myOrders/order/delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        })
+
+        // read manageOrders
+        app.get('/manageOrders', async (req, res) => {
+            const result = await ordersCollection.find({}).toArray();
+            res.send(result)
+        })
+
+        //order delete from  allOrders
+        app.delete('/allOrders/order/delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        })
+
+        // update order status
+        app.put('/orderStatus/update/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const updateDoc = {
+                $set: { status: 'Shipped' }
+            };
+            const result = await ordersCollection.updateOne(filter, updateDoc)
+            res.json(result)
+        })
+
+        // read manage Service
+        app.get('/manageService', async (req, res) => {
+            const result = await servicesCollection.find({}).toArray();
+            res.send(result)
+        })
+
+        //Service delete from  all Service
+        app.delete('/alService/service/delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+        })
+
+
+
+
+
 
 
     }
